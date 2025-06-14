ef80ab8633435998c8405f717c6a8917e0a5809f
ondarigidion <gidion.ondari@student.moringaschool.com>
2025-06-14 18:16:12 +0300

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+// Function expression called divide
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function called square
+const square = (num) => num * num;
+
+// Arrow function called add
+const add = (a, b) => a + b;
+
+module.exports = {
+  divide,
+  square,
+  add
+};
